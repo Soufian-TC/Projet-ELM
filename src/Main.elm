52dfeb36894d212651c265acb9ad0f7b8cfb71cc
Ilhan Ghauri <ilhanghauri@Ilhans-MacBook-Air.local>
2025-01-28 15:40:43 +0100

    Code

---
+++ src/Main.elm
@@ -0,0 +1,60 @@
+module Main exposing (..)
+import Browser
+import Html exposing (Html, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { content : String
+  }
+
+
+init : Model
+init =
+  { content = "" }
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Change String
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change newContent ->
+      { model | content = newContent }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ div [] -- Premier div
+        [ text ("Type in your code below:") ]
+    , div [] -- Deuxième div
+        [ input [ placeholder "Text to reverse", value model.content, onInput Change ] []
+        , div [] [ text (String.reverse model.content) ]
+        ]
+    ]
+
+  
